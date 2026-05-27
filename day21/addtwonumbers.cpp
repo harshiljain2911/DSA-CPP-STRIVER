5e7d11ba339b2f5fc6da97e4f9718e8e0989f04a
Harshil Jain <kotaharshil2906@gmail.com>
2026-05-27 11:35:01 +0530

    Add Two Numbers in Linked List
    Create a dummy node that will act as the starting point of the new linked list.
    Create a temporary pointer and set it to the dummy node.
    Start with a carry value of 0.
    Loop through both linked lists until you reach the end of both, or until there is no carry left.
    At each step, add the values of the current nodes and the carry.
    Update the carry by dividing the total by 10.
    Create a new node with the last digit of the total (total % 10) and attach it to the next of the temporary pointer, then move the temporary pointer forward.
    Move both list pointers to their next nodes.
    After the loop ends, return the next node of the dummy (this is the head of the result list).
    The dummy node is used to make the code easier. Without it, extra conditions would be needed to handle the first node.

---
+++ day21/addtwonumbers.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+
+        int carry = 0;
+        
+        ListNode dummy(0);
+        ListNode *ans = &dummy;
+
+        while(temp1!=NULL || temp2!=NULL || carry)
+        {
+            int sum = carry;
+
+            if(temp1!=NULL)
+            {
+                sum+=temp1->val;
+                temp1=temp1->next;
+            }
+            if(temp2!=NULL)
+            {
+                sum+=temp2->val;
+                temp2 = temp2->next;
+            }
+
+            int digit = sum % 10;
+            carry = sum / 10;
+            ans->next = new ListNode(digit);
+
+            ans = ans->next;
+
+        }
+
+        return dummy.next;
+
+    }
+  
+};
