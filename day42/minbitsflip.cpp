d0acbba2c3219e43fe5741f54a1d3c7d9ebd8892
Harshil Jain <kotaharshil2906@gmail.com>
2026-06-17 13:27:58 +0530

    Minimum Bit Flips to Convert Number
    - To find how many bits differ between two numbers, compare them bit by bit.
    - XOR highlights the positions where the two numbers have different bits.
    - Each set bit in the XOR result represents a required flip to match the numbers.
    - Count the number of set bits in the XOR result to get the total flips needed.
    - This provides the minimum number of bit changes to convert one number into the other.

---
+++ day42/minbitsflip.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSetBits(int n) {
+        int count = 0;
+        while(n>0)
+        {
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+
+    int minBitFlips(int start, int goal) {
+        return countSetBits(start^goal);
+    }
+};
