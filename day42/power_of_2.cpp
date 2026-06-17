01e11d7c768d08ced44fce635e00493e1cce787d
Harshil Jain <kotaharshil2906@gmail.com>
2026-06-17 09:23:13 +0530

    Check if a Number is Power of 2 or Not

---
+++ day42/power_of_2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int countSetBits(int n) {
+        int count = 0;  // Variable to store the count of set bits
+
+        // Step 1: While n is non-zero, turn off the rightmost set bit
+        while (n>0) {
+            n &= (n - 1);  // Turn off the rightmost set bit
+            count++;  // Increment the count
+        }
+
+        // Step 2: Return the count of set bits
+        return count;
+    }
+
+    bool isPowerOfTwo(int n) {
+        return countSetBits(n)==1;
+    }
+};
