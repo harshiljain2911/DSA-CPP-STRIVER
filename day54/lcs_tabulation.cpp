b83f19c9d391105f55f1ae56d1628cbec0700d31
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-02 14:53:01 +0530

    Longest Common Subsequence Tabulation
    - Declare a `dp` array of size [`n`][`m`], where `n` and `m` are the lengths of strings `S1` and `S2` respectively. As there are two changing parameters in the recursive solution, `ind1` and `ind2`, the maximum value `ind1` can attain is `n` and `ind2` can attain a maximum value of `m`. Hence, we need a 2D `dp` array.
    - **Setting Base Cases in the Array:**
        - In the recursive code, the base condition is when `ind1 < 0` or `ind2 < 0`, but we cannot set the dp array’s index to -1. To handle this, we shift every index by 1 towards the right.
        - Now, the base case becomes `if(ind1 == 0 || ind2 == 0)`, return 0. So, we set the dp array with 0 for `ind1 = 0` and `ind2 = 0`.
    - **Iterative Computation Using Loops:**
        - Initialize two nested for loops to traverse the `dp` array.
        - Following the logic from the recursive approach, set the value of each cell in the 2D `dp` array. Instead of recursive calls, use the `dp` array itself to find the values of intermediate calculations.
        - Keep in mind the shifting of indices, for example, `S1[ind1]` will be converted to `S1[ind1 - 1]`.
    - **Returning the Answer:**
        - Finally, `dp[n][m]` will hold the solution after completing the entire process, as we are doing the calculations in a bottom-up manner.

---
+++ day54/lcs_tabulation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int lcs(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int t=0;t<=m;t++) dp[0][t]=0;
+        for(int t=0;t<=n;t++) dp[t][0]=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+            
+                if(str1[i-1]==str2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                
+            }
+        }
+
+
+        return dp[n][m];
+
+    }
+};
