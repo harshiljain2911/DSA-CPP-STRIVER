32ba8dd9ee070c2b249683ca17cb2648e0519fcb
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-02 15:56:31 +0530

    Longest common substring Space Optimised
    - If we observe the relation in the tabulation approach, we use `dp[i-1][j-1]` to calculate the present row. This means we are only using the previous row, `dp[ind1-1][ ]`, to compute the current row.
    - Since we only need the previous row to calculate the current row, we don't need to maintain the entire 2D `dp` array. Instead, we can use just two rows: `prev` (corresponding to `dp[ind-1]`) and `cur` (corresponding to `dp[ind]`).
    - After declaring `prev` and `cur`, replace `dp[ind-1]` with `prev` and `dp[ind]` with `cur` in the tabulation code.
    - Following the same logic as the tabulation approach, we will calculate `cur[ind]` and store the maximum of all values in the `ans` variable during each iteration. After the inner loop completes, set `prev = cur`, so that `cur` can serve as the previous row for the next iteration.
    - At the end of the execution, return `ans` as the answer. It will contain the maximum length of the longest common substring.

---
+++ day54/lcsubstring_space_optimised.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int longestCommonSubstr(string str1, string str2) {
+
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+
+                if (str1[i - 1] == str2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = 0;
+
+                ans = max(ans, curr[j]);
+            }
+
+            prev = curr;
+        }
+
+        return ans;
+    }
+};
