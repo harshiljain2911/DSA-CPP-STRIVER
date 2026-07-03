1744e753d1c46a03b95d2f8e0ac4d9d53ae7584e
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-03 15:42:01 +0530

    Delete Operation for Two Strings Space Optimisation
    Same as LCS but returning sum of lengths of both text strings and subtracting twice the length of lcs

---
+++ day55/delete_op_two_strings_memoized.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+     int fun(int i,int j,string & s1, string & s2,vector<vector<int>> & dp) {
+        
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s1[i]==s2[j]) return dp[i][j]=1+fun(i-1,j-1,s1,s2,dp);
+
+        else return dp[i][j]=max(fun(i-1,j,s1,s2,dp),fun(i,j-1,s1,s2,dp));
+    
+    }
+
+    int lcs(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return fun(n-1,m-1,str1,str2,dp);
+    }
+
+    int minDistance(string word1, string word2) {
+
+        int len = lcs(word1, word2);
+
+        return word1.length() + word2.length() - 2 * len;
+    }
+};
