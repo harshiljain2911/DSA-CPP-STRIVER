56999321c7cca9b53900dd12bfc9705c092b9ce6
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-07 16:05:45 +0530

    Best time to buy and sell stock II Tabulation
    To change the memoization method into a tabulation method, we create a table (dp array) of the same size. We fill it step by step instead of using recursion.
    
    - First, we make a table with rows for each day and two columns for the two possible states (buy or sell). Initially, all values are set to zero.
    - We then set the base condition: for the last day (when no more trading is possible), both buying and selling give zero profit.
    - Next, we use two loops:
        - The outer loop moves from the second last day back to the first day.
        - The inner loop goes through both states (buying and selling).
    - For every day and state, we fill the table using the same choice logic as in the memoization method (choosing the maximum profit between doing nothing and making a transaction).
    - Finally, the answer is stored in the first cell, which gives the maximum profit starting from the first day when buying is allowed.

---
+++ day58/best_time_to_buy_sell_stocks_II_tabulation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+
+        long long profit=0;
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for (int i = n-1; i >=0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                if (j) {
+                    profit = max(-prices[i] + dp[i+1][0],
+                                 dp[i+1][1]);
+                } else {
+                    profit = max(prices[i] + dp[i+1][1],
+                                 dp[i+1][0]);
+                }
+
+                dp[i][j] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
