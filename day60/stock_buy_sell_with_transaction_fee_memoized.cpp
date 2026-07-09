a8f6828595973d64a2b2cb84d696dc6d70cc5024
Harshil Jain <kotaharshil2906@gmail.com>
2026-07-09 10:10:55 +0530

    Best time to buy and sell stock with transaction fees Memoized
    Each day gives two choices, either do nothing and continue, or take an action based on what was done last. We aim to explore all possible paths to find the highest profit, using recursion to try each path.
    
    We think in terms of passing through each day. On any given day, we may be in a state where we're allowed to buy or we must sell before buying again. This helps us decide what action is allowed.
    
    If we're allowed to buy, we can either:
    
    - Skip buying and move to the next day, gaining nothing now.
    - Buy the stock, which costs money, this reduces our total profit. The next opportunity will then be a chance to sell.
    
    If we must sell before buying again, we can either:
    
    - Skip selling and move to the next day. Nothing changes.
    - Sell the stock, which adds to our profit. After selling, we’re again allowed to buy. A transaction fee is subtracted at this point because the complete transaction is now done.
    
    Buying and selling together count as one full transaction. The fee is only subtracted after the selling part.
    
    We always look to choose the option that gives the higher profit.

---
+++ day60/stock_buy_sell_with_transaction_fee_memoized.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long fun(int i,int buy,vector<int>& prices,int fee,vector<vector<int>>& dp){
+        
+        long long profit = 0;
+
+        if(i==prices.size()) return 0;
+    
+        if(dp[i][buy]!=-1) return dp[i][buy];
+
+        if(buy)
+        {
+            profit = max(-prices[i]-fee + fun(i+1,0,prices,fee,dp),fun(i+1,1,prices,fee,dp));
+        }
+        else
+        {
+            profit = max(prices[i] + fun(i+1,1,prices,fee,dp),fun(i+1,0,prices,fee,dp));
+        }
+
+        return dp[i][buy] = profit;
+    
+    }
+
+
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int n = prices.size();
+
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+
+        return fun(0,1,prices,fee,dp);
+
+    }
+};
